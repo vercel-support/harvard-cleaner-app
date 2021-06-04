57c182802e01ce0b772381f279334064179e35e3
Yummypampers2 <chris.er.weekes@gmail.com>
2021-06-04 10:27:23 -0700

    Shopping Cart useState

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,68 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import products from '../products.json';
 
 import { initiateCheckout } from '../lib/payments.js';
 
+import products from '../products.json';
+
+const defaultCart = {
+  products: {}
+}
+
 export default function Home() {
-  console.log('NEXT_PUBLIC_STRIPE_API_KEY', process.env.NEXT_PUBLIC_STRIPE_API_KEY)
+
+  const [cart, updateCart] = useState(defaultCart);
+
+  const cartItems = Object.keys(cart.products).map(key => {
+    const product = products.find(({ id }) => `${id}` === `${key}`);
+    return {
+      ...cart.products[key],
+      pricePerItem: product.price
+
+    }
+  });
+
+  const subtotal = cartItems.reduce((accumulator, { pricePerItem, quantity }) => {
+    return accumulator + (pricePerItem * quantity)
+  }, 0)
+
+  const totalItems = cartItems.reduce((accumulator, { quantity }) => {
+    return accumulator + quantity
+  }, 0)
+
+
+  function addToCart({ id } = {}) {
+    updateCart(prev => {
+      let cartState = { ...prev };
+
+      if (cartState.products[id]) {
+        cartState.products[id].quantity = cartState.products[id].quantity + 1;
+      } else {
+        cartState.products[id] = {
+          id,
+          quantity: 1
+        }
+      }
+
+      return cartState;
+    })
+  }
+
+
+  function checkout() {
+    initiateCheckout({
+      lineItems: cartItems.map(item => {
+        return {
+          price: item.id,
+          quantity: item.quantity
+        }
+      })
+    });
+  }
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -23,6 +79,15 @@ export default function Home() {
           Let's get you all cleaned up with environmentally-friendly cleaning services
         </p>
 
+        <p className={styles.description}>
+          <strong>Items:</strong> {totalItems}
+          <br />
+          <strong>Total Cost:</strong> ${subtotal}
+          <br />
+          <button className={styles.button} onClick={checkout}>Check Out</button>
+        </p>
+
+
         <ul className={styles.grid} >
           {products.map(product => {
             const { id, title, price, description, image } = product;
@@ -36,15 +101,11 @@ export default function Home() {
                 </a>
                 <p>
                   <button className={styles.button} onClick={() => {
-                    initiateCheckout({
-                      lineItems: [
-                        {
-                          price: id,
-                          quantity: 1
-                        }
-                      ]
-                    });
-                  }}>Buy This!</button>
+                    addToCart({
+                      id
+                    })
+
+                  }}>Add To Cart</button>
                 </p>
               </li>
             )
